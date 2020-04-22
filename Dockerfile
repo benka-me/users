7d336d9c252565b965a07057d043bcd0f1aa8304
benka-me <newcratie@gmail.com>
2020-04-22 16:46:13 +0200

    docker and env

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM golang
+RUN go get github.com/benka-me/users
--- go-pkg/http/rpc/auth.go
+++ go-pkg/http/rpc/auth.go
@@ -2,12 +2,14 @@ package rpc
 
 import (
 	"context"
+	"fmt"
 	"github.com/benka-me/users/go-pkg/jwt"
 	"github.com/benka-me/users/go-pkg/users"
 )
 
 func (app *App) Auth(ctx context.Context, req *users.Token) (*users.IsAuth, error) {
 	err := jwt.CheckJwt(req.Val)
+	fmt.Println("auth err:", err, req.Val)
 
-	return &users.IsAuth{Val: err == nil}, err
+	return &users.IsAuth{Val: err == nil}, nil
 }
